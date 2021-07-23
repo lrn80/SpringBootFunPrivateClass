b91f0e8446ad27a22e6306b09a3adb19efe5a6c3
李若宁 <liruoning@liruoningdeMacBook-Pro.local>
2021-07-23 14:06:09 +0800

    feat: pathVariable注解

--- src/main/java/com/lrn/springboot/ParamController.java
+++ src/main/java/com/lrn/springboot/ParamController.java
@@ -2,6 +2,7 @@ package com.lrn.springboot;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,19 @@ public class ParamController {
 
     @GetMapping("/requestparam")
     public User RequestParam(@RequestParam String name, @RequestParam int age){
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        return user;
+    }
+
+    @GetMapping("/requestparam2")
+    public String RequestParam(@RequestParam(value="na", required = false, defaultValue = "李若宁") String name){
+        return "Hello: " + name;
+    }
+
+    @GetMapping("/pathvariable/{name}/{age}")
+    public User PathVariable(@PathVariable String name, @PathVariable int age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
